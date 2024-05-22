c138ae78f2a46efcdabc087e72014b00b4e7bc46
navyachiluka <116788280+navyachiluka@users.noreply.github.com>
2024-05-22 10:20:28 -0700

    Update RippleCarryAdder.v

--- RippleCarryAdder.v
+++ RippleCarryAdder.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Chiluka Navya
 // 
 // Create Date: 24.02.2024 22:03:32
 // Design Name: 
@@ -30,4 +30,24 @@ fulladder g1(S[0],C[0],A[0],B[0],C0);
 fulladder g2(S[1],C[1],A[1],B[1],C[0]);
 fulladder g3(S[2],C[2],A[2],B[2],C[1]);
 fulladder g4(S[3],Carry,A[3],B[3],C[2]);
+endmodule
+
+//test bench for the Ripple Carry Adder
+module RippleCarryAdder_tb;
+reg A,B;
+wire S;
+wire Carry;
+//instantiating design block
+RippleCarryAdder uut(
+.A(A),
+.B(B),
+.S(S),.Carry(Carry));
+initial begin
+A=4'b1001;
+B=4'b1010;
+# 10 
+A=4'1100;
+B=4'1111;
+#10 $stop;
+end
 endmodule
